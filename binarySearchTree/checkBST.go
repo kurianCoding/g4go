5b99081f2515713001f3d93474e32120cf7cae39
kurian <kurianck.mail@gmail.com>
2018-10-06 18:54:02 +0530

    check if binary search tree
        function takes in one as parameter and returns true or false

---
+++ binarySearchTree/checkBST.go
@@ -0,0 +1,13 @@
+package bst
+
+/*
+   given a tree, find if the tree is a binary search tree
+*/
+func IsBst(root *Node) bool {
+	/*
+		this function takes root node and returns a bool
+		it will return a true if the tree is a binary search
+		tree and false if its not
+	*/
+	return true
+}
