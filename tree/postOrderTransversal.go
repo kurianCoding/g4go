2634732f259e16167185bd34774b4d618aec7fbe
kurian <kurianck.mail@gmail.com>
2018-10-19 15:28:26 +0530

    starting test

--- tree/postOrderTransversal.go
+++ tree/postOrderTransversal.go
@@ -5,30 +5,3 @@ package tree
    then print the right subtree and then finally print
    the root
 */
-func search(arr []int, x, n int) int {
-	/*
-		return the index
-	*/
-	for i := 0; i < len(arr); i++ {
-		if arr[i] == x {
-			return i
-		}
-	}
-	return -1
-}
-func PrintPostOrder(pre, in []int, res *[]int) {
-	/*
-	   this function takes in pre order and in order transversals
-	   of the tree and returns a slice which is the post order
-	   transversal.
-	*/
-	root := search(in, pre, n)
-	if root != 0 {
-		PrintPostOrder(in, pre+1, root, res)
-	}
-	if root != n-1 {
-		PrintPostOrder(in+root, pre+root, n-root-1, res)
-	}
-	(*res) = append((*res), pre[0])
-	return
-}
